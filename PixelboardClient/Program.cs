e096e0a455bf64d00803d882835ec2d3d2e3c20d
VLADO\zivan <vladimir.zivanovic@bwz-rappi.ch>
2026-01-21 14:53:04 +0100

    Modifed Programm.cs to use new Models

--- PixelboardClient/Program.cs
+++ PixelboardClient/Program.cs
@@ -1,26 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Razor Pages hinzufügen
 builder.Services.AddRazorPages();
+
+// HttpClient für PixelboardService registrieren
+builder.Services.AddHttpClient<PixelboardClient.Services.PixelboardService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorPages();
 
 app.Run();
