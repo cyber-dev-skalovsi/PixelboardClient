ed3ef936755caabfdfaabab8e54b221339f65bec
VLADO\zivan <vladimir.zivanovic@bwz-rappi.ch>
2026-02-17 02:28:14 +0100

    Milestone 3

---
+++ PixelboardClient/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿public interface IAuthService
+{
+    bool IsAuthenticated { get; }
+    string Username { get; }
+}
+
+public class AuthService : IAuthService
+{
+    private readonly IHttpContextAccessor _contextAccessor;
+
+    public AuthService(IHttpContextAccessor contextAccessor)
+    {
+        _contextAccessor = contextAccessor;
+    }
+
+    public bool IsAuthenticated => _contextAccessor.HttpContext.User.Identity.IsAuthenticated;
+    public string Username => _contextAccessor.HttpContext.User.Identity.Name ?? "Gast";
+}
